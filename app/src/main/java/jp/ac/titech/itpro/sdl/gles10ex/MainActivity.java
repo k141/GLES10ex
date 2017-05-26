1a9cf9c21a9665cd78d3554ded33e882c5f2b013
koji <ishii4itsp@gmail.com>
2017-05-26 16:56:22 +0900

    Accel Rotation

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
@@ -1,5 +1,9 @@
 package jp.ac.titech.itpro.sdl.gles10ex;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,12 +11,17 @@ import android.util.Log;
 import android.widget.SeekBar;
 
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, SensorEventListener {
     private final static String TAG = "MainActivity";
 
     private GLSurfaceView glView;
     private SimpleRenderer renderer;
     private SeekBar rotationBarX, rotationBarY, rotationBarZ;
+
+    private SensorManager sensorMgr;
+    private Sensor accelerometer;
+    private float rx, ry, rz;
+    private final static float alpha = 0.75F;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         renderer.addObj(new Pyramid(0.5f, 0, 0, 0));
         renderer.addObj(new Octahedron(0.5f, 1, 1, 1));
         glView.setRenderer(renderer);
+
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     @Override
@@ -40,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onResume();
         Log.d(TAG, "onResume");
         glView.onResume();
+        sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -67,4 +80,17 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        rx = alpha * rx + (1 - alpha) * event.values[0];
+        ry = alpha * ry + (1 - alpha) * event.values[1];
+        rz = alpha * rz + (1 - alpha) * event.values[2];
+        renderer.setRotationX(5 * rx);
+        renderer.setRotationY(5 * ry);
+        renderer.setRotationZ(5 * rz);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 }
